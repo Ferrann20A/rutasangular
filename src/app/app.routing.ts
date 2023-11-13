54fb382d3199953ba9734885867cdc640927449c
Ferrann20A <alumno.398783@ies-azarquiel.es>
2023-11-13 10:22:55 +0100

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,29 @@
+import { HomeComponent } from "./components/home/home.component";
+import { CineComponent } from "./components/cine/cine.component";
+import { MusicaComponent } from "./components/musica/musica.component";
+//Necesitamos los siguientes modulos para realizar la declaracion de navegacion
+import { Routes, RouterModule } from "@angular/router";
+import {ModuleWithProviders} from '@angular/core';
+import { Error404Component } from "./components/error404/error404.component";
+
+//Definimos un array con nuestras rutas para los components
+const appRoutes: Routes = [
+    {
+        path:"", component: HomeComponent
+    },
+    {
+        path:"cine", component: CineComponent
+    },
+    {
+        path:"musica", component: MusicaComponent
+    },
+    {
+        path:"**", component: Error404Component
+    }
+]
+
+//Desde routing debemos exportar dos caracteristicas para implementarlas dentro de module
+//1. El proveedor de rutas
+export const appRoutingProvider: any[] = [];
+//2. El modulo routing indicando el array de rutas declarado
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
